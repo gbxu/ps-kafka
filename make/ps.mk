38a0c2811fc68576cc8447efede4dfc33f05308d
gbxu@Cathy <gb.xu@outlook.com>
2018-04-23 16:41:14 +0800

    modify makefile

--- make/ps.mk
+++ make/ps.mk
@@ -9,5 +9,5 @@ ifeq ($(USE_KEY32), 1)
 ADD_CFLAGS += -DUSE_KEY32=1
 endif
 
-PS_LDFLAGS_SO = -L$(DEPS_PATH)/lib -lprotobuf-lite -lrdkafka -lz -lpthread -lrt
+PS_LDFLAGS_SO = -L$(DEPS_PATH)/lib -lprotobuf-lite -lrdkafka -lz -lpthread -lrt -ldl
 PS_LDFLAGS_A = $(addprefix $(DEPS_PATH)/lib/, libprotobuf-lite.a librdkafka.a)
