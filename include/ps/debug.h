f378d10a34489b49e5a7b689cdedb25d5e9a88fb
gbxu@Cathy <gb.xu@outlook.com>
2018-05-02 09:56:15 +0800

    fix:TOSCHDULER =》 TOSCHEDULER

--- include/ps/debug.h
+++ include/ps/debug.h
@@ -11,7 +11,7 @@ server_id:8, 10, 12, …
 #ifndef PSKAFKA_DEBUG_H
 #define PSKAFKA_DEBUG_H
 
-#define DEBUGORNOT true
+#define DEBUGORNOT false
 #define DEBUGPUSHPULL false
 #include "ps/internal/van.h"
 
